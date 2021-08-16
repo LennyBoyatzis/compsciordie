c1df91d3be6fdb14f6c6fa0aa4cec876ab695e4d
Lenny Boyatzis <lennyboyatzis@gmail.com>
2021-08-17 09:02:18 +1000

    add tournament winner question

---
+++ app/problems/arrays_and_strings/tournament_winner.py
@@ -0,0 +1,19 @@
+from collections import defaultdict
+
+# n = number of competitions/games
+# O(n) runtime
+# O(n) space to maintain scores for n games
+
+def tournamentWinner(competitions, results):
+	score_board = defaultdict(int)
+	
+	for idx, game in enumerate(competitions):
+		home_team, away_team = game
+		home_win = results[idx] == 1
+		if home_win:
+			score_board[home_team] += 3
+		else:
+			score_board[away_team] += 3
+	
+	winner = max(score_board.items(), key=lambda x: x[1])
+	return winner[0]
