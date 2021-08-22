1d426fa9e5003bfda7c33c3a576654b6810f93e4
Lenny Boyatzis <lennyboyatzis@gmail.com>
2021-08-22 15:36:42 +1000

    add test cases for string decompression question

--- app/problems/recursion/string_decompression.py
+++ app/problems/recursion/string_decompression.py
@@ -44,21 +44,21 @@ def decompress_string(text: str) -> str:
 
 
 if __name__ == '__main__':
-    # test_one_input = '3[a]2[bc]'
-    # test_one_expected = 'azazbb'
-    # print(f'one: {decompress_string(test_one_input)}')
+    test_one_input = '3[a]2[bc]'
+    test_one_expected = 'azazbb'
+    print(f'one: {decompress_string(test_one_input)}')
 
-    # test_two_input = '2[3[a]c]'
-    # test_two_expected = 'aaac' * 2
-    # print(f'two: {decompress_string(test_two_input)}')
+    test_two_input = '2[3[a]c]'
+    test_two_expected = 'aaac' * 2
+    print(f'two: {decompress_string(test_two_input)}')
 
-    # test_three_input = 'a[]b'
-    # test_three_expected = 'ab'
-    # print(f'three: {decompress_string(test_three_input)}')
+    test_three_input = 'a[]b'
+    test_three_expected = 'ab'
+    print(f'three: {decompress_string(test_three_input)}')
 
-    # test_four_input = '0[abc]'
-    # test_four_expected = ''
-    # print(f'four: {decompress_string(test_four_input)}')
+    test_four_input = '0[abc]'
+    test_four_expected = ''
+    print(f'four: {decompress_string(test_four_input)}')
 
     test_five_input = 'abc3[cd]xyz'
     test_five_expected = ''
