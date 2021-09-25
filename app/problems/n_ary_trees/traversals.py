2a9af9043a284830e3945245123af7a25fb7dd0b
Lenny Boyatzis <lennyboyatzis@gmail.com>
2021-09-25 15:03:06 +1000

    add level order traversal with sublists for levels

--- app/problems/n_ary_trees/traversals.py
+++ app/problems/n_ary_trees/traversals.py
@@ -20,6 +20,30 @@ def bfs_levelorder_traversal(root):
 
     return path
 
+
+def bfs_levelorder_traversal_tiered(root):
+    if not root:
+        return []
+
+    curr_queue, next_queue = None, deque([root])
+    levels = []
+
+    while next_queue:
+        curr_queue = next_queue
+        next_queue = deque([])
+        level = []
+
+        while curr_queue:
+            node = curr_queue.pop()
+            if node:
+                level.append(node.value)
+
+                for child in node.children:
+                    next_queue.appendleft(child)
+        levels.append(level)
+    return levels
+
+
 # Visit root node first, then children nodes
 def dfs_preorder_traversal(root):
     path = []
@@ -90,3 +114,6 @@ if __name__ == '__main__':
 
     level_order = bfs_levelorder_traversal(root)
     print(f'level_order {level_order}') # A->B->C->D->E->F->G. 
+
+    level_order_tiered = bfs_levelorder_traversal_tiered(root)
+    print(f'level_order_tiered {level_order_tiered}') # [A]->[B->C->D]->[E->F->G]. 
